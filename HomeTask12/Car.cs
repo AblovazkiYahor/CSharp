f710d3eee478a680aad2bf7a9f146bf27cb0476d
AblovazkiYahor <ablovacky12@gmail.com>
2023-11-08 23:41:05 +0300

    Add new task with cars

---
+++ HomeTask12/Car.cs
@@ -0,0 +1,57 @@
+﻿namespace HomeTask12
+{
+    public class Car
+    {
+        //Поля класса
+        private string color;
+        private string type;
+        private int year;
+
+        //Конструктор класса
+        public Car(string color, string type, int year)
+        {
+            this.color = color;
+            this.type = type;
+            this.year = year;
+        }
+
+        //Метод для запуска автомобиля
+        public void StartCar()
+        {
+            Console.WriteLine("Автомобиль заведён");
+        }
+
+        //Метод для отключения автомобиля
+        public void StopCar()
+        {
+            Console.WriteLine("Автомобиль заглушен");
+        }
+
+        //Метод для установки года выпуска автомобиля
+        public void SetYear(int year)
+        {
+            this.year = year;
+        }
+
+        //Метод для установки типа автомобиля
+        public void SetType(string type)
+        {
+            this.type = type;
+        }
+
+        //Метод для установки цвета автомобиля
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        //Метод для получения информации о машине
+        public void GetCarInfo()
+        {
+            Console.WriteLine($"Год выпуска: {year}");
+            Console.WriteLine($"Тип: {type}");
+            Console.WriteLine($"Цвет: {color}");
+        }
+    }
+}
+
---
+++ HomeTask12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HomeTask12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем объект класса Car
+            Car myCar = new Car("Белый", "Седан", 2011);
+
+            //Выводим информацию о машине
+            Console.WriteLine("Информация о моей машине:");
+            myCar.GetCarInfo();
+
+            //Запускаем и отключаем машину
+            myCar.StartCar();
+            myCar.StopCar();
+
+            //Устанавливаем новый год выпуска, тип и цвет
+            myCar.SetYear(2023);
+            myCar.SetType("Кроссовер");
+            myCar.SetColor("Синий");
+
+            //Выводим обновленную информацию о машине
+            Console.WriteLine("Информация о моей машине после обновления:");
+            myCar.GetCarInfo();
+        }
+    }
+}
+
