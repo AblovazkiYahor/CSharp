6dc950099db8050f3fe91cbbe4200bd6a798f329
AblovazkiYahor <ablovacky12@gmail.com>
2023-11-09 12:23:06 +0300

    Creating Soda Class

---
+++ HomeTask10_Soda/SodaClass.cs
@@ -0,0 +1,24 @@
+﻿public class Soda
+{
+    private string flavor;
+
+    //Конструктор класса Soda, принимающий параметр "вкус"
+    public Soda(string flavor = null)
+    {
+        this.flavor = flavor;
+    }
+
+    //Метод для получения строковой репрезентации
+    public string GetDescription()
+    {
+        if (!string.IsNullOrEmpty(flavor))
+        {
+            return $"У вас газировка с {flavor} вкусом";
+        }
+        else
+        {
+            return "У вас обычная газировка";
+        }
+    }
+}
+
