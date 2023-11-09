52dfe11e25c9d621c32c309fa88dd74b893cf850
AblovazkiYahor <ablovacky12@gmail.com>
2023-11-09 12:33:03 +0300

    Creating Sphere Class

---
+++ HomeTask13_Sphere/ClassMain.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Sphere sphere1 = new Sphere(); //Создаем сферу с единичным радиусом и центром в начале координат
+        Sphere sphere2 = new Sphere(3); //Создаем сферу с радиусом 3 и центром в начале координат
+        Sphere sphere3 = new Sphere(2, 1, 2, 3); //Создаем сферу с радиусом 2 и заданными координатами центра
+
+        //Изменяем координаты центра сферы
+        sphere1.SetCenter(1, 2, 3);
+
+        //Проверяем, находится ли точка внутри сферы
+        double testX = 2;
+        double testY = 2;
+        double testZ = 2;
+        bool isInside = sphere2.IsPointInside(testX, testY, testZ);
+
+        Console.WriteLine("Сфера 1: Радиус = " + sphere1.Radius);
+        Console.WriteLine("Сфера 2: Радиус = " + sphere2.Radius);
+        Console.WriteLine("Сфера 3: Радиус = " + sphere3.Radius + ", Центр = " + sphere3.GetCenter());
+        Console.WriteLine("Объем сферы 2: " + sphere2.GetVolume());
+        Console.WriteLine("Площадь поверхности сферы 2: " + sphere2.GetSquare());
+
+        if (isInside)
+        {
+            Console.WriteLine($"Точка ({testX}, {testY}, {testZ}) находится внутри сферы 2.");
+        }
+        else
+        {
+            Console.WriteLine($"Точка ({testX}, {testY}, {testZ}) не находится внутри сферы 2.");
+        }
+    }
+}
+
---
+++ HomeTask13_Sphere/SphereClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Sphere
+{
+    private double radius;
+    private double x;
+    private double y;
+    private double z;
+
+    //Конструкторы
+    public Sphere()
+    {
+        radius = 1;
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Sphere(double radius)
+    {
+        this.radius = radius;
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Sphere(double radius, double x, double y, double z)
+    {
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //Свойство Radius
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+
+    //Метод для вычисления объема сферы
+    public double GetVolume()
+    {
+        return (4 / 3) * Math.PI * Math.Pow(radius, 3);
+    }
+
+    //Метод для вычисления площади внешней поверхности сферы
+    public double GetSquare()
+    {
+        return 4 * Math.PI * Math.Pow(radius, 2);
+    }
+
+    //Метод для получения координат центра сферы
+    public string GetCenter()
+    {
+        return $"{x} {y} {z}";
+    }
+
+    //Метод для изменения координат центра сферы
+    public void SetCenter(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //Метод для проверки, находится ли точка внутри сферы
+    public bool IsPointInside(double testX, double testY, double testZ)
+    {
+        double distance = Math.Sqrt(Math.Pow(testX - x, 2) + Math.Pow(testY - y, 2) + Math.Pow(testZ - z, 2));
+        return distance <= radius;
+    }
+}
+
+
+
