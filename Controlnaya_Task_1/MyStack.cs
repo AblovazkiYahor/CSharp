85a547fa2cb2dc58cabe5943c9c9e1089454a982
AblovazkiYahor <ablovacky12@gmail.com>
2023-11-13 21:48:40 +0300

    Задача 1 из контрольной работы

---
+++ Controlnaya_Task_1/MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class MyStack
+{
+    private int[] array;
+    private int top; //Индекс верхнего элемента стека
+    private int capacity; //Максимальная вместимость стека
+
+    public MyStack(int capacity)
+    {
+        if (capacity <= 0)
+        {
+            throw new ArgumentException("Емкость стека должна быть больше 0.");
+        }
+
+        this.capacity = capacity;
+        this.array = new int[capacity];
+        this.top = -1; // Пустой стек
+    }
+
+    public void Push(int item)
+    {
+        if (top < capacity - 1)
+        {
+            top++;
+            array[top] = item;
+        }
+        else
+        {
+            Console.WriteLine("Переполнение стека. Невозможно отправить элемент: " + item);
+        }
+    }
+
+    public int Pop()
+    {
+        if (top >= 0)
+        {
+            int poppedItem = array[top];
+            top--;
+            return poppedItem;
+        }
+        else
+        {
+            Console.WriteLine("Переполнение стека. Невозможно извлечь из пустого стека.");
+            return -1; //Значение по умолчанию, можно изменить в зависимости от требований.
+        }
+    }
+}
+
+
+
---
+++ Controlnaya_Task_1/RunnerStack.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Пример использования MyStack
+        MyStack stack = new MyStack(5);
+
+        //Добавление элементов в стек
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        //Удаление элементов из стека
+        Console.WriteLine(stack.Pop()); //Выведет 3
+        Console.WriteLine(stack.Pop()); //Выведет 2
+        Console.WriteLine(stack.Pop()); //Выведет 1
+
+        //Попытка удалить из пустого стека
+        Console.WriteLine(stack.Pop()); //Выведет сообщение об ошибке
+
+        //Добавление элементов после удаления
+        stack.Push(4);
+        stack.Push(5);
+
+        //Попытка добавить в полный стек
+        stack.Push(6); //Выведет сообщение об ошибке
+    }
+}
