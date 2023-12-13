cdde7c858b0dd609ae508c7583116d49a2368726
AblovazkiYahor <ablovacky12@gmail.com>
2023-12-13 19:09:34 +0300

    HomeWork_10.1

---
+++ HomeTask16_Input/Input_count.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        //Путь к входному файлу
+        string inputFilePath = "input.txt";
+
+        // Путь к выходному файлу
+        string outputFilePath = "output.txt";
+
+        try
+        {
+            // Считываем все строки из входного файла
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            // Создаем или перезаписываем выходной файл
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                // Обрабатываем каждую строку
+                foreach (string line in lines)
+                {
+                    // Получаем самое частое слово и количество его повторений
+                    var mostCommonWordInfo = GetMostCommonWord(line);
+
+                    // Записываем результат в выходной файл
+                    writer.WriteLine($"{mostCommonWordInfo.Word} {mostCommonWordInfo.Count}");
+                }
+            }
+
+            Console.WriteLine("Результат записан в файл: " + outputFilePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+    }
+
+    // Метод для нахождения самого частого слова и его количества повторений в строке
+    static (string Word, int Count) GetMostCommonWord(string input)
+    {
+        // Разбиваем строку на слова
+        string[] words = input.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Создаем словарь для подсчета повторений каждого слова
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        // Подсчитываем количество повторений каждого слова
+        foreach (string word in words)
+        {
+            if (wordCount.ContainsKey(word))
+            {
+                wordCount[word]++;
+            }
+            else
+            {
+                wordCount[word] = 1;
+            }
+        }
+
+        // Находим самое частое слово и его количество повторений
+        var mostCommonWord = wordCount.OrderByDescending(pair => pair.Value).FirstOrDefault();
+
+        return (mostCommonWord.Key, mostCommonWord.Value);
+    }
+}
+
+
